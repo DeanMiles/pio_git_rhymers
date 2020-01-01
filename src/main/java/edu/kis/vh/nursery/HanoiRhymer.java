eba51ba89478d0b9fbb63341a1a46621a8439ee0
DeanMiles <sevirion@gmail.com>
2020-01-01 23:48:38 +0100

    'reformat'

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,17 +1,17 @@
 package edu.kis.vh.nursery;
-
+//	Kombinacja klawiszy pozwala przesuwac sie po otworzonych plikach
 public class HanoiRhymer extends defaultCountingOutRhymer {
 
-int totalRejected = 0;
+    int totalRejected = 0;
 
-	public int reportRejected() {
-		return totalRejected;
-	}
+    public int reportRejected() {
+        return totalRejected;
+    }
 
-	public void countIn(int in) {
-	if (!callCheck() && in > peekaboo())
-			totalRejected++;
-			else
-				super.countIn(in);
-	}
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
 }
